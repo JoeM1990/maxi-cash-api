a9885ea9b01413370bc8acc995a9f415f9108e34
Joe Monkila <jnkiwa25@gmail.com>
2024-07-20 10:05:12 +0100

    payement controller

--- src/main/java/com/monkilatech/maxi_cash_api/controller/PayementController.java
+++ src/main/java/com/monkilatech/maxi_cash_api/controller/PayementController.java
@@ -1,5 +1,40 @@
 package com.monkilatech.maxi_cash_api.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
 public class PayementController {
-    
+    @PostMapping("/paynow")
+    public ResponseEntity<String> payNow(@RequestBody Map<String, Object> paymentRequest) {
+        try {
+            String url = "https://api-testbed.maxicashapp.com/Merchant/api.asmx/PayNowSynch";
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String payloadString = objectMapper.writeValueAsString(paymentRequest).replace("\"", "\\\"");
+
+            String requestBody = "{ \"strData\": \"" + payloadString + "\" }";
+
+            HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+            return response;
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("An error occurred: " + e.getMessage());
+        }
+    }
 }
