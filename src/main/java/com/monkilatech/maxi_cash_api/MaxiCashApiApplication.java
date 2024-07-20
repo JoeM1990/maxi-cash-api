0ec862d6a92debb4d3836876fb6a90fa2290a09e
Joe Monkila <jnkiwa25@gmail.com>
2024-07-20 10:02:18 +0100

    init commit

---
+++ src/main/java/com/monkilatech/maxi_cash_api/MaxiCashApiApplication.java
@@ -0,0 +1,13 @@
+package com.monkilatech.maxi_cash_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxiCashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxiCashApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monkilatech/maxi_cash_api/MaxiCashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkilatech.maxi_cash_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxiCashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
